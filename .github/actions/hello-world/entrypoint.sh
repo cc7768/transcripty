e2105f5a8a6ae3cecf92883c70750d03c23c3f1c
Chase <cc7768@gmail.com>
2019-12-07 03:32:37 -0500

    bleh

--- .github/actions/hello-world/entrypoint.sh
+++ .github/actions/hello-world/entrypoint.sh
@@ -2,7 +2,7 @@
 
 echo "Hello $1"
 time=$(date)
-sv=$(sphinx --version)
+sphinx_version=$(sphinx --version)
 echo ::set-output name=time::$time
-echo ::set-output name=sphinx_version::$sv
+echo ::set-output name=sphinx_version::$sphinx_version
 
