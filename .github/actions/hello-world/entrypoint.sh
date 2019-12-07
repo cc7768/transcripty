f87206d59f1a4e6694ecf416294956f51f0e958d
Chase <cc7768@gmail.com>
2019-12-07 03:39:24 -0500

    Another run

--- .github/actions/hello-world/entrypoint.sh
+++ .github/actions/hello-world/entrypoint.sh
@@ -2,7 +2,7 @@
 
 echo "Hello $1"
 time=$(date)
-sphinx_version=$(sphinx --version)
+sphinx_version=$(python -c "import sphinx; print(sphinx.__version__)")
 echo ::set-output name=time::$time
 echo ::set-output name=sphinx_version::$sphinx_version
 
